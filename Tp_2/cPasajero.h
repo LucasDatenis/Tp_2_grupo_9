ca919925c8b4b98297ca6276c87dce14fc0b79aa
LucasDatenis <lucasdatenis@users.noreply.github.com>
2022-04-08 14:28:24 -0300

    Creacion de las clases

---
+++ Tp_2/cPasajero.h
@@ -0,0 +1,17 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+class Pasajero {
+private:
+	string DNI;
+	string nombre;
+	string apellido;
+	unsigned int nrovuelo;
+	string fecha;
+};
